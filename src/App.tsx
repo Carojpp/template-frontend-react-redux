9c05edc3a3e8fb859757beb2b750b472d9f483d8
Edwin Antonio Arroyo Lopez <zeroedprogrammer@gmail.com>
2023-12-13 12:43:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState({});
+
+  useEffect(() => {
+    (async function () {
+      try {
+        const result: any = await axios.get("http://localhost:7000/users", {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json;charset=utf-8",
+          },
+        });
+        const { data } = result
+        console.log({ data });
+        setUsers(data);
+      } catch (e:any) {
+        console.error(e);
+        setError(e)
+      }
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {users.length > 0 ? users.map((user:any) => 
+          <div>
+            <p>Name: {user.name}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>   
+            <hr></hr>
+          </div>) : <div>No hay usuarios todavia</div>}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
